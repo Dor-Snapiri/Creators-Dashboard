bec635ab8b1fbea8214590f4163b9858a8737f30
Dor Snapiri <dsnapiri@lightricks.com>
2025-09-02 16:51:59 +0300

    Avatar: Initial commit
    - Dynamic Avatar component
    - SCSS styling

---
+++ src/Avatar/Avatar.tsx
@@ -0,0 +1,40 @@
+import './Avatar.scss'
+import AvatarIcon from '../assets/defaultAvatarIcon.svg?react'
+
+
+type Size = 'xxxs' | 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxxl';
+type AavatarConfig =
+    | { type: 'image'; url: string }
+    | { type: 'initials'; firstName: string; lastName: string }
+    | { type: 'icon' };
+
+type AvatarProps = { size: Size, config: AavatarConfig }
+
+
+
+export default function Avatar({ size, config }: AvatarProps) {
+    let content: React.ReactNode;
+
+    switch (config.type) {
+        case 'image':
+            content = <img
+                className="image"
+                src={config.url}
+                alt="avatar"
+            />;
+            break;
+        case 'initials':
+            const initials = `${config.firstName[0]}${config.lastName[0]}`.toUpperCase();
+            content = <div className="initials" data-size={size}>{initials}</div>;
+            break;
+        case 'icon':
+            content = <div className="icon"><AvatarIcon /></div>;
+            break;
+    }
+
+    return (
+        <div className={'container'} data-size={size}>
+            {content}
+        </div>
+    )
+}
