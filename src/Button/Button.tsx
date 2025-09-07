f6b34a84ba34500d6080f68c2aedff82c076c94b
Dor Snapiri <dsnapiri@lightricks.com>
2025-09-07 09:12:24 +0300

    Button: fix
    - added prop type
    - used data-* to pass variables to scss file, simplfying the tsx code
    - moved svg to seperate file
    - added default values

---
+++ src/Button/Button.tsx
@@ -0,0 +1,46 @@
+import './Button.scss'
+
+
+type ButtonProps = {
+    mode?: 'filled' | 'tinted' | 'plain' | 'elevated';
+    size?: 'small' | 'medium' | 'large';
+    onClick?: () => void
+} & (
+        | {
+            composition: 'text';
+            text: string;
+
+        }
+        | {
+            composition: 'icon';
+            icon: React.ReactElement<SVGSVGElement>;
+        }
+        | {
+            composition: 'iconFirst' | 'textFirst';
+            text: string;
+            icon: React.ReactElement<SVGSVGElement>;
+        }
+    )
+
+export default function Button(props: ButtonProps) {
+    const { mode = 'tinted', size = 'medium', onClick, composition } = props;
+
+    function renderContent() {
+        switch (composition) {
+            case 'icon':
+                return props.icon;
+            case 'text':
+                return props.text;
+            case 'iconFirst':
+                return <>{props.icon}<span>{props.text}</span></>;
+            case 'textFirst':
+                return <><span>{props.text}</span>{props.icon}</>;
+        }
+    };
+
+    return (
+        <button className='ltButton' onClick={onClick} data-mode={mode} data-size={size} data-composition={composition}>
+            {renderContent()}
+        </button>
+    );
+}
