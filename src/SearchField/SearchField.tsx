519cdf5d26be0549a7f09af443057cf6c6f40869
Dor Snapiri <127883230+Dor-Snapiri@users.noreply.github.com>
2025-09-09 11:27:09 +0300

    SearchField: Initial Implementation
    Stylized search field.
    
    <img width="828" height="310" alt="image" src="https://github.com/user-attachments/assets/f31f6933-6d78-490d-9b98-2ac7f9db8cd6" />

---
+++ src/SearchField/SearchField.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import './SearchField.scss'
+import MagnifyingGlassIcon from '../assets/magnifyingGlassIcon.svg?react'
+import CloseIcon from '../assets/closeIcon.svg?react'
+
+type SearchFieldProps = {
+    placeholder?: string;
+    onChange: (value: string) => void;
+};
+
+export default function SearchField({ placeholder = "Search", onChange }: SearchFieldProps) {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+        onChange(event.target.value);
+    }
+
+    return (
+        <form>
+            <div className='inputContainer'>
+                <MagnifyingGlassIcon />
+                <input
+                    type="text"
+                    placeholder={placeholder}
+                    value={inputValue}
+                    onChange={handleInputChange}
+                />
+            </div>
+
+            {inputValue.trim() && (
+                <button className='clearButton' onClick={() => setInputValue('')}>
+                    <CloseIcon />
+                </button>
+            )}
+        </form>
+    );
+}
