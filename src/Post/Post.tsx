853b0b27115a6c71c4ea3334da7545dc9adeb347
Dor Snapiri <dsnapiri@lightricks.com>
2025-09-09 16:37:01 +0300

    Post: made comments more abstract - left icon can now be anything instread of initials avatar

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -1,26 +1,25 @@
 import './Post.scss'
-import Avatar from '../Avatar/Avatar';
 import ArrowIcon from '../assets/ArrowForwardIcon.svg?react';
 import { useState } from 'react';
 
 type CommentProps = {
-    name: { firstName: string; lastName: string };
+    leftIcon: React.ReactNode;
     content: string;
 }
 
 type PostProps = {
-    url: string;
+    coverPhotoUrl: string;
     comments: CommentProps[];
 }
 
-export default function Post({ url, comments }: PostProps) {
+export default function Post({ coverPhotoUrl, comments }: PostProps) {
     const [isToggled, setIsToggled] = useState<boolean>(false);
 
     return (
         <div className="container">
 
             <div className="post">
-                <img src={url} alt="post" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                <img src={coverPhotoUrl} alt="post photo" />
 
                 <button className="commentToggle" onClick={() => setIsToggled(!isToggled)}>
                     <span className='postText'> {isToggled ? 'Hide' : 'View'} {comments.length} comments </span>
@@ -31,14 +30,7 @@ export default function Post({ url, comments }: PostProps) {
             <div className="commentContainer">
                 {isToggled && comments.map((comment, idx) => (
                     <div className="comment" key={idx}>
-                        <Avatar
-                            size="sm"
-                            config={{
-                                type: 'initials',
-                                firstName: comment.name.firstName,
-                                lastName: comment.name.lastName
-                            }}
-                        />
+                        {comment.leftIcon}
                         <span className="postText">{comment.content}</span>
                     </div>
                 ))}
