f86562f54c9d473fa0595e54926d779f11a7912a
Dor Snapiri <dsnapiri@lightricks.com>
2025-09-09 11:25:08 +0300

    SearchField: fix
    - name is no longer a prop
    - clear button now shows up only once a character other than a whitespace has been typed

--- src/SearchField/SearchField.tsx
+++ src/SearchField/SearchField.tsx
@@ -4,36 +4,30 @@ import MagnifyingGlassIcon from '../assets/magnifyingGlassIcon.svg?react'
 import CloseIcon from '../assets/closeIcon.svg?react'
 
 type SearchFieldProps = {
-    name: string
     placeholder?: string
 };
 
-export default function SearchField({ name, placeholder = "Search" }: SearchFieldProps) {
+export default function SearchField({ placeholder = "Search" }: SearchFieldProps) {
     const [inputValue, setInputValue] = useState('');
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(event.target.value)
     }
 
-    const handleClear = () => {
-        setInputValue('')
-    }
-
     return (
-        <form name={name}>
+        <form>
             <div className='inputContainer'>
                 <MagnifyingGlassIcon />
                 <input
                     type="text"
                     placeholder={placeholder}
-                    name={`${name}Input`}
                     value={inputValue}
                     onChange={handleInputChange}
                 />
             </div>
 
-            {inputValue && (
-                <button className='clearButton' onClick={handleClear}>
+            {inputValue.trim() && (
+                <button className='clearButton' onClick={() => setInputValue('')}>
                     <CloseIcon />
                 </button>
             )}
